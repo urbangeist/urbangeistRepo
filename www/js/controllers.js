07cbce3f08e0c3c33ccefcbece8091b9b6f6d884
charlesbones <bones.charles@gmail.com>
2016-03-03 00:01:58 +0100

    json information

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,16 +5,24 @@ app.controller('howtoCtrl', function($scope) {
 app.controller('introCtrl', function($scope) {
 	$scope.ghostName = "CeciliaMofo";
 })
-app.controller('bio', function($scope) {
-	$scope.ghostName = "CeciliaMofo";
+app.controller('bio', function($scope, ListFactory) {
+    ListFactory.then(function(response){
+        $scope.ghostName = response.ghosts[0].name
+        $scope.ghostIntro=response.ghosts[0].introduction
+    })
 })
-app.controller('questCtrl', function($scope) {
-	$scope.ghostIntroduction = "This is my Ghost introduction..";
+app.controller('questCtrl', function($scope,ListFactory) {
+    ListFactory.then(function(response){
+        $scope.questTitle = response.ghosts[0].tasks[0].questTitle
+        $scope.questIntro = response.ghosts[0].tasks[0].questIntro
+    })
 })
 
-app.controller('questDescCtrl', function($scope) {
-	$scope.questTitle = "Where the pink rose lays, there will be blood..";
-  $scope.questTip = "I need to find the Salvation Army.";
+app.controller('questDescCtrl', function($scope,ListFactory) {
+    ListFactory.then(function(response){
+        $scope.questTitle = response.ghosts[0].tasks[0].questTitle
+        $scope.questTip = response.ghosts[0].tasks[0].questIntro
+    })
 })
 
 app.controller('userCtrl', function($scope,globalValues) {
@@ -29,34 +37,44 @@ app.controller('introCtrl', function($scope) {
   $scope.ghostName = "CeciliaMofo";
 })
 
-
-
-app.controller('historicalCtrl', function($scope,globalValues) {
-	$scope.items=globalValues.ghosts;
+app.controller('historicalCtrl', function($scope, ListFactory) {
+    ListFactory.then(function(response){
+        $scope.items = response.ghosts
+    })
 });
+
+
+//app.controller('historicalCtrl', function($scope,globalValues) {
+  //$scope.items=globalValues.obj;
+//});
 
 app.controller('ceciliaCtrl', function($scope) {
 	
 });
 
 app.controller('accomplishedCtrl', function($scope,globalValues) {
-	$scope.items=globalValues.ghosts;
+	$scope.items=globalValues.obj;
 });
 
-app.controller('hintCtrl', function($scope,$ionicPopup, $timeout) {
+app.controller('hintCtrl', function($scope,$ionicPopup, $timeout,ListFactory) {
+  ListFactory.then(function(response){
   $scope.showAlert = function() {
-   var alertPopup = $ionicPopup.alert({
-     title: 'Hint #1',
-     template: 'It might taste good'
-   });
-   alertPopup.then(function(res) {
-     console.log('Thank you for not eating my delicious ice cream cone');
-   });
- };
+     var alertPopup = $ionicPopup.alert({
+       title: 'Hint #1',
+       template: response.ghosts[0].tasks[0].hint[0].message
+     });
+     alertPopup.then(function(res) {
+       console.log('Thank you for not eating my delicious ice cream cone');
+     });
+   }
+  })
   $scope.hintNo = "2";
 })
 
 app.controller("fetchData", function($scope, $http) {
+  ListFactory.then(function(response){
+        $scope.items = response.ghosts
+    })
 $scope.ghosts= [];
 $http.get('json/data.json').success(function(data) { 
     console.log("success!");
